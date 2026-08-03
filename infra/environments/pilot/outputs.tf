53d0bc06b8f731f5ce4ed612a721f52efceb4ef3
Todd Brunia <tbrunia@gmail.com>
2026-08-03 16:31:41 -0500

    Add protected pilot provisioning guardrails (#16)

--- infra/environments/pilot/outputs.tf
+++ infra/environments/pilot/outputs.tf
@@ -1,3 +1,7 @@
 output "ecr_repository_url" { value = aws_ecr_repository.worker.repository_url }
 output "public_subnet_ids" { value = values(aws_subnet.public)[*].id }
 output "isolated_subnet_ids" { value = values(aws_subnet.isolated)[*].id }
+output "application_secret_arns" {
+  value = { for name, secret in aws_secretsmanager_secret.application : name => secret.arn }
+}
+output "runtime_secret_policy_arn" { value = aws_iam_policy.runtime_secrets.arn }
